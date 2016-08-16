2c053be42dec666edfc38e3618e027f1913127b0
gokula2010 <gokula2010@yahoo.com>
2016-08-16 22:19:15 +0800

    spotify service added and used in search component

--- app/components/search/search.component.ts
+++ app/components/search/search.component.ts
@@ -1,13 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+import { SpotifyService } from '../../services/spotify.service';
 
 @Component({
     moduleId: module.id,
     selector: 'search',
-    template: '<h1>search.component.html</h1>'
+    templateUrl: 'search.component.html',
+    providers: [SpotifyService]
 })
 export class SearchComponent implements OnInit {
-    constructor() { }
+    searchStr: string;
+    searchRes: Artist[];
+
+    constructor(private _spotifyService: SpotifyService) {
+
+    }
 
     ngOnInit() { }
+
+    searchMusic() {
+        console.log(this.searchStr);
+        this._spotifyService.searchMusic(this.searchStr)
+            .subscribe(res=> {
+                //console.log(res.artists.items);
+                this.searchRes = res.artists.items;
+            })
+    }
+}
+
+export class Album {
+    id: number;
+    name: string;
+
+}
+
+export class Artist {
+    id: number;
+    name: string;
+    genres: any;
+    albums: Album[];
 
 }
